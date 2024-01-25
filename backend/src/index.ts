548fb412e7a31aee54b1985ba7836f464c3dc1e0
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2024-01-25 15:04:43 +0000

    add recipe id

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import * as RecipeAPI from './recipe-api'
+import { PrismaClient } from '@prisma/client'
 
 
 const app = express();
+const prismaClient = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
@@ -25,6 +27,54 @@ app.get("/api/recipes/:recipeId/summary", async (req, res) => {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+// favourite recipe
+app.post("/api/recipes/favourite", async (req, res) => {
+  const recipeId = req.body.recipeId;
+  const userId = req.body.userId;
+  try {
+    const favouriteRecipe = await prismaClient.favouriteRecipes.create({
+      data: {
+        recipeId,
+        userId,
+      },
+    });
+    return res.status(201).json(favouriteRecipe);
+  } catch (error) {
+    console.log( error);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+});
+// Remove from favourites
+app.delete("/api/recipes/favourite/:id", async (req, res) => {
+  const recipeId = req.body.recipeId;
+  try {
+     await prismaClient.favouriteRecipes.delete({
+      where: {
+        recipeId: recipeId,
+      },
+    });
+    return res.status(204).send();
+  } catch (error) {
+    console.log( error);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+});
+// Get favourite recipes
+app.get("/api/recipes/favourite", async (req, res) => {
+  try {
+    const favouriteRecipes = await prismaClient.favouriteRecipes.findMany();
+    // Map through the array and get only the recipe's data
+    const favouriteRecipeIds = favouriteRecipes.map((favouriteRecipe) => favouriteRecipe.recipeId.toString());
+    const favourites = await RecipeAPI.getFavouriteRecipeByIDs(favouriteRecipeIds);
+    return res.json(favourites);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+});
+// Get favourite recipes by 
+
 
 
 app.listen(5100, () => {
--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -49,5 +49,45 @@ export const getRecipeSummary = async (recipeId: string) => {
   }
 };
 
+export const getRecipeInformation = async (recipeId: string) => {
+  if (!apiKey) {
+    throw new Error("API Key not found");
+  }
+
+  const url = new URL(`https://api.spoonacular.com/recipes/${recipeId}/information`);
+
+  const queryParams = {
+    apiKey,
+  };
+  url.search = new URLSearchParams(queryParams).toString();
+
+  try {
+    const searchResponse = await fetch(url);
+    const resultsJson = await searchResponse.json();
+    return resultsJson;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//favourite recipe by id
+
+export const getFavouriteRecipeByIDs = async (ids: string[]) => {
+  if (!apiKey) {
+    throw new Error("API Key not found");
+  }
+  const url = new URL("https://api.spoonacular.com/recipes/informationBulk");
+
+  const params = {
+    apiKey,
+    ids: ids.join(","),
+  };
+  url.search = new URLSearchParams(params).toString();
+  const response = await fetch(url);
+  const json = await response.json();
+
+  return { results:json };
+
+};
 
 
