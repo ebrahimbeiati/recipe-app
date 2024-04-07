db74d2f340e3b19d95cc5d2173417f287ff0904d
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2024-04-07 20:28:29 +0100

    edit1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (_, res) => {
-  res.send("Hello Ebrahim janam!");
+  res.send("Hello Ebrahim janamm!");
 })
 
 // Endpoint to search recipes
